3b9685913bb73418aa00a9c63e3c184a58feb2f3
Mathieu Escouteloup <mathieu.escoueloup@ims-bordeaux.fr>
2025-07-02 10:05:29 +0200

    Implement with: RAM:24x4bytes / Density:72% / Tile:4x2 + Update UART pin

--- src/Top.v
+++ src/Top.v
@@ -17,16 +17,25 @@ module tt_um_emmk_riscv (
 );
     
   // Default
-  assign uo_out[7:1]  = ui_in[7:2] + uio_in[7:2];
   wire _unused = &{ena, 7'b000000};
 
+  assign uo_out[0] = ui_in[0] | uio_in[0];
+  assign uo_out[1] = ui_in[1] | uio_in[1];
+  assign uo_out[2] = ui_in[2] | uio_in[2];
+  assign uo_out[3] = ui_in[4] | uio_in[3];
+//  assign uo_out[4] = ui_in[4] | uio_in[4];
+  assign uo_out[5] = ui_in[5] | uio_in[5];
+  assign uo_out[6] = ui_in[6] | uio_in[6];
+  assign uo_out[7] = ui_in[7] | uio_in[7];
+
+  // Chisel design
   Sys m_riscV(
     .clock          (clk        ),
     .reset          (!rst_n     ),
      
-    .io_i_boot      (ui_in[1]   ),
-    .io_b_uart_rx   (ui_in[0]   ),
-    .io_b_uart_tx   (uo_out[0]  ),
+    .io_i_boot      (ui_in[0]   ),
+    .io_b_uart_rx   (ui_in[3]   ),
+    .io_b_uart_tx   (uo_out[4]  ),
      
     .io_b_gpio_eno  (uio_oe     ),
     .io_b_gpio_out  (uio_out    ),
